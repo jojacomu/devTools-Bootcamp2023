e9ab58b77f5bd978d37939618a080c659745c1c5
johnCortes <jojacomu@gmail.com>
2023-06-05 10:31:32 -0500

    Adiciono ejercicio01.js

---
+++ ejercicio01.js
@@ -0,0 +1,25 @@
+// Cálculo 
+function next_pal(numero) {
+    function esPalindromo(num) {
+      const str = num.toString();
+      const longitud = str.length;
+      for (let i = 0; i < longitud / 2; i++) {
+        if (str[i] !== str[longitud - 1 - i]) {
+          return false;
+        }
+      }
+      return true;
+    }
+  
+    let siguienteNumero = numero + 1;
+    while (!esPalindromo(siguienteNumero)) {
+      siguienteNumero++;
+    }
+    return siguienteNumero;
+  }
+  
+  // Ejemplo de uso
+  const numeroInicial = 11;
+  const siguientePalindromo = next_pal(numeroInicial);
+  console.log(`El siguiente número palíndromo después de ${numeroInicial} es: ${siguientePalindromo}`);
+
